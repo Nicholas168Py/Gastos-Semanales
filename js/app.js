53aeb15b156825604282885bb4cc2f46ac10184e
Nicholas168Py <nicolasrochaacosta@gmail.com>
2026-02-11 11:48:18 -0500

    Muestro los gastos en HTML

--- js/app.js
+++ js/app.js
@@ -23,7 +23,12 @@ class Presupuesto {
 
     nuevoGasto(gasto){
         this.gastos = [...this.gastos, gasto];
-        console.log(this.gastos);
+        this.calcularRestante();
+    }
+
+    calcularRestante(){
+        const gastado = this.gastos.reduce( (total, gasto) => total + gasto.cantidad, 0);
+        this.restante = this.presupuesto - gastado;
     }
 }
 
@@ -63,11 +68,38 @@ class UI{
     }
 
     agregarGastoListado(gastos){
+
+        this.limpiarHTML(); //Elimina el HTML previo
         
         //Iterar sobre los gastos
         gastos.forEach(gasto => {
-            console.log(gasto);
+            const {cantidad, nombre, id} = gasto;
+
+            //Crear un LI
+            const nuevoGasto = document.createElement('li');
+            nuevoGasto.className = 'list-group-item d-flex justify-content-between align-items-center';
+            nuevoGasto.dataset.id = id;
+            console.log(nuevoGasto);
+
+            //Agregar el HTML del gasto
+            nuevoGasto.innerHTML = `${nombre} <span class="badge badge-primary badge-pill">$ ${cantidad} </span>
+            `;
+
+            // Boton para borrar el gasto
+            const btnBorrar = document.createElement('button');
+            btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
+            btnBorrar.innerHTML = 'Borrar &times';
+            nuevoGasto.appendChild(btnBorrar);
+
+            //Agregar al HTML
+            gastoListado.appendChild(nuevoGasto);
         });
+    }
+
+    limpiarHTML() {
+        while(gastoListado.firstChild) {
+            gastoListado.removeChild(gastoListado.firstChild)
+        }
     }
 }
 
